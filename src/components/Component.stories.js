d2ad11e19a311bc3ee891396839d42b5faf6ed45
Alberto Zaranza <albertozaranza@outlook.com>
2022-03-21 18:45:29 -0300

    Create test component

---
+++ src/components/Component.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+
+import Component from './Component';
+
+storiesOf('Test Component', module).add('example', () => <Component />);
---
+++ src/components/Component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Component: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello from React Native</Text>
+    </View>
+  );
+};
+
+export default Component;
